2150186ce2825f102016764fe8f332a8409947db
zhangzhixiong <957329715@qq.com>
2019-02-18 20:36:06 +0800

    用户管理开发完毕

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,18 +17,19 @@
           background-color="#333744"
           text-color="#fff"
           active-text-color="#409EFF"
-          :style="menushow?'width:65px;':'width:200px;'"
           :collapse="menushow"
           :collapse-transition="false"
           :unique-opened="true"
+          :router="true"
         >
-          <el-submenu :index="item.id+''" v-for="(item, k) in menuList" :key="item.id">
+          <el-submenu :index="item.id+''" v-for="(item, k) in menuList" :key="item.id"
+          :style="menushow?'width:65px;':'width:200px;'">
             <template slot="title">
               <i :class="'iconfont icon-' + menuicon[k]"></i>
               <span>{{item.authName}}</span>
             </template>
             <el-menu-item
-              :index="item.id+'-'+item2.id"
+              :index="item2.path"
               v-for="item2 in item.children"
               :key="item2.id"
             >
--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,20 @@ import Router from 'vue-router'
 import Login from '@/components/Login'
 import Home from '@/components/Home'
 import Welcome from '@/components/Welcome'
+import User from '@/components/User'
 
 Vue.use(Router)
 
 var router = new Router({
-  routes: [
-    { path: '/login', component: Login },
+  routes: [{path: '/login', component: Login},
     {
       path: '/home',
       component: Home,
       redirect: '/welcome',
-      children: [{ path: '/welcome', component: Welcome }]
+      children: [
+        {path: '/welcome', component: Welcome},
+        {path: '/users', component: User}
+      ]
     }
   ]
 })
